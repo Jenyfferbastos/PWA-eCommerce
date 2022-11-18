59de5164c6555beea4d821bc3eb5a951dabdb05f
Jenyffer <jenyfferbastos06@gmail.com>
2022-11-18 15:50:59 -0300

    criei uma função que muda todos os arquivos do tipo svg para components icons

--- src/components/Icons/Icons.tsx
+++ src/components/Icons/Icons.tsx
@@ -1,19 +1,24 @@
-import handBangsOrange from '../../assets/img/productIconsImages/handbags-orange.svg';
-import handBags from '../../assets/img/productIconsImages/handbags.svg';
-import jewelleryOrange from '../../assets/img/productIconsImages/jewellery-orange.svg';
-import jewellery from '../../assets/img/productIconsImages/jewellery.svg';
-import skincareOrange from '../../assets/img/productIconsImages/skincare-orange.svg';
-import skincare from '../../assets/img/productIconsImages/skincare.svg';
-import watchOrange from '../../assets/img/productIconsImages/watch-orange.svg';
-import watch from '../../assets/img/productIconsImages/watch.svg';
+import { generalIcons } from "./GeneralIcons";
+import { navigationIcons } from "./NavigationIcons";
+import { productIcons } from "./ProductIcons";
 
-export const productIcons = {
-  ['handBangsOrange']: handBangsOrange,
-  ['handBags']: handBags,
-  ['jewelleryOrange']: jewelleryOrange,
-  ['jewellery']: jewellery,
-  ['skincareOrange']: skincareOrange,
-  ['skincare']: skincare,
-  ['watchOrange']: watchOrange,
-  ['watch']: watch,
+export interface IconsProps{
+  icon: string;
 }
+
+export function Icons({icon}: IconsProps){
+    return(
+      <>
+      <img src={determinesIcons(icon)} />
+      </>
+    );
+}
+
+function determinesIcons(icon:string):string {
+
+const allIcons = {
+  ...generalIcons, ...navigationIcons, ...productIcons
+}
+  return allIcons[icon];
+}
+
