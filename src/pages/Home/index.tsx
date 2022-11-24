3c9693fe9ab59368ad9aef9d22c6691591a35df0
Jenyffer <jenyfferbastos06@gmail.com>
2022-11-24 10:06:48 -0300

    adicionei os novos components na página Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,20 @@
+import { BannerAlert } from "../../components/BannerAlert/BannerAlert";
+import { HandpickedCollections } from "../../components/HandpickedCollections/HandpickedCollections";
 import { Header } from "../../components/Header/Header";
+import { MainCarousel } from "../../components/MainBanner/MainBanner";
+import { ProductCard } from "../../components/ProductCard/ProductCard";
+import { TopCategories } from "../../components/TopCategories/TopCategories";
 
 export function Home() {
 
   return (
     <>
       <Header />
+      <BannerAlert />
+      <MainCarousel />
+      <TopCategories />
+      <ProductCard />
+      <HandpickedCollections />
     </>
   );
 }
