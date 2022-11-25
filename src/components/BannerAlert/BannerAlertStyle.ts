994b76cc42e09a249dfc7629c4f73deb27d1b869
Jenyffer <jenyfferbastos06@gmail.com>
2022-11-25 10:25:11 -0300

    refatorei os banners

--- src/components/BannerAlert/BannerAlertStyle.ts
+++ src/components/BannerAlert/BannerAlertStyle.ts
@@ -1,21 +1,21 @@
-import styled from 'styled-components'
-import { mediaQueries } from '../../utils/mediaQueries'
+import styled from "styled-components";
+import { mediaQueries } from "../../utils/mediaQueries";
 
 export const BannerAlertStyle = styled.div`
-  ${mediaQueries.desktop}{
+  margin-top: 80px;
+  ${mediaQueries.desktop} {
     height: 50px;
-    background-color: #F0F0F0;
+    background-color: #f0f0f0;
     color: #171520;
     font-size: 14px;
     display: flex;
     justify-content: center;
 
-    p{
+    p {
       align-self: center;
     }
-
   }
-  ${mediaQueries.mobile}{
+  ${mediaQueries.mobile} {
     display: none;
   }
-`
+`;
--- src/components/MainBanner/MainBannerStyle.ts
+++ src/components/MainBanner/MainBannerStyle.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { mediaQueries } from "../../utils/mediaQueries";
 import { px2vw } from "../../utils/px2vw";
 
 export const MainBannerStyle = styled.div`
@@ -10,6 +11,11 @@ export const MainBannerStyle = styled.div`
   margin-top: ${px2vw(20)};
   position: relative;
 
+  ${mediaQueries.mobile} {
+    img {
+      margin-top: 70px;
+    }
+  }
   img {
     width: 97vw;
     border-radius: ${px2vw(16)};
@@ -18,14 +24,21 @@ export const MainBannerStyle = styled.div`
 export const TextBanner = styled.div`
   background: rgba(222, 222, 222, 0.7);
   position: absolute;
-  top:${px2vw(50)};
-  right: ${px2vw(22)};
   z-index: 2;
-  color: #1B4B66;
+  color: #1b4b66;
   width: ${px2vw(700)};
-  padding:  ${px2vw(30)};
+  padding: ${px2vw(30)};
   border-top-left-radius: ${px2vw(30)};
   border-bottom-left-radius: ${px2vw(30)};
+
+  ${mediaQueries.mobile} {
+    top: 80px;
+    right: ${px2vw(22)};
+  }
+  ${mediaQueries.desktop} {
+    top: ${px2vw(50)};
+    right: ${px2vw(22)};
+  }
 
   h2 {
     font-size: ${px2vw(80)};
