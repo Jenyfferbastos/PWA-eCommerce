ecd5bb3c6ddea94a68235f01f4cf751f95b08600
Jenyffer <jenyfferbastos06@gmail.com>
2022-11-25 10:23:02 -0300

    refatorei o component Product Card

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -3,10 +3,19 @@ import ImagemBag1 from "../../assets/img/image-bag-1.png";
 import ImagemBag2 from "../../assets/img/image-bag-2.png";
 import ImagemBag3 from "../../assets/img/image-bag-3.png";
 import ImagemBag4 from "../../assets/img/image-bag-4.png";
-import { ProductCardStyle } from "./ProductCardStyle";
+import { ContainerNavStyle, ProductCardStyle } from "./ProductCardStyle";
+import { Icons } from "../Icons/Icons";
 
 export function ProductCard() {
   return (
+    <>
+    <ContainerNavStyle>
+    <h2>New Arrivals</h2>
+    <button>View All
+      <Icons icon={'ChevronRight'} />
+    </button>
+    </ContainerNavStyle>
+    
     <ProductCardStyle>
       <ModelProductCard nameBag="Grande" descritionBag="Blossom Pouch" valueBag="$39.49" img={ImagemBag1} />
 
@@ -16,5 +25,6 @@ export function ProductCard() {
 
       <ModelProductCard nameBag="Boujee" descritionBag="Black Bag" valueBag="$56.49" img={ImagemBag4} />
     </ProductCardStyle>
+    </>
   );
 }
--- src/components/ProductCard/ProductCardStyle.ts
+++ src/components/ProductCard/ProductCardStyle.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { mediaQueries } from "../../utils/mediaQueries";
 import { px2vw } from "../../utils/px2vw";
 
 export const ProductCardStyle = styled.div`
@@ -15,5 +16,40 @@ export const ProductCardStyle = styled.div`
   ::-webkit-scrollbar {
     width: 0px;
   }
-
   `;
+
+  export const ContainerNavStyle =  styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-left: ${px2vw(30)};
+    margin-top: ${px2vw(30)};
+    margin-right: ${px2vw(30)};
+
+    ${mediaQueries.desktop} {
+    h2{
+      font-size: ${px2vw(32)};
+    }
+    button{
+      font-size: ${px2vw(20)};
+    }
+  }
+  ${mediaQueries.mobile} {
+    h2{
+      font-size: ${px2vw(66)};
+    }
+    button{
+      font-size: ${px2vw(56)};
+    }
+  }
+
+    button{
+      background-color: transparent;
+      border: none;
+      align-self: center;
+      display: flex;
+      align-items: center;
+      color: #1B4B66;
+      
+    }
+  `
