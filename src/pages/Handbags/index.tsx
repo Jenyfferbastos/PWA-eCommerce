276b9acb770215482e4034f8d9a3fa4a0ebc9598
Jenyffer <jenyfferbastos06@gmail.com>
2022-11-27 18:28:30 -0300

    inseri novos components no index da page handbags

--- src/pages/Handbags/index.tsx
+++ src/pages/Handbags/index.tsx
@@ -1,14 +1,27 @@
 import { BannerHandbags } from "../../components/BannerHandbags/BannerHandbags";
+import { CardHorizontal } from "../../components/CardHorizontal/CardHorizontal";
 import { Header } from "../../components/Header/Header";
-import { RadioCheckbox } from "../../components/RadioCheckbox/RadioCheckbox";
 import { SelectCheckbox } from "../../components/SelectCheckbox/SelectCheckbox";
+import ImagemBag1 from "../../assets/img/image-bag-1.png";
+import ImagemBag2 from "../../assets/img/image-bag-2.png";
+import ImagemBag3 from "../../assets/img/image-bag-3.png";
+import ImagemBag4 from "../../assets/img/image-bag-4.png";
+import { ProductListing } from "./Handbags";
+import { Flex } from "../../components/ProductCard/ModelProductCardStyle";
 
 export function Handbags(){
   return(
     <>
     <Header />
     <BannerHandbags />
+    <Flex >
     <SelectCheckbox  TextButton={"teste"} Icon={"Plus"}  />
+    <ProductListing>
+    <CardHorizontal img={ImagemBag1} nameBag={"Grande"} descritionBag={"Blossom Pouch"} valueBeforeBag={"$39.49"} valueAfterBag={"$78.66"} valueDiscountBag={"50% OFF"} rating={"43 Ratings"} />
+    <CardHorizontal img={ImagemBag2} nameBag={"Grande"} descritionBag={"Blossom Pouch"} valueBeforeBag={"$39.49"} valueAfterBag={"$78.66"} valueDiscountBag={"50% OFF"} rating={"43 Ratings"} />
+    <CardHorizontal img={ImagemBag3} nameBag={"Grande"} descritionBag={"Blossom Pouch"} valueBeforeBag={"$39.49"} valueAfterBag={"$78.66"} valueDiscountBag={"50% OFF"} rating={"43 Ratings"} />
+    </ProductListing>
+    </Flex>
     </>
   )
 }
