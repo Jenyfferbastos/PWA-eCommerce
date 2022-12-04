0ec567c75f34fcf9ffb4014e395281112de5a3fe
Jenyffer <jenyfferbastos06@gmail.com>
2022-12-04 20:40:29 -0300

    refatorei a estilização dos componentes da rota Handbags

--- src/pages/Handbags/Handbags.ts
+++ src/pages/Handbags/Handbags.ts
@@ -1,18 +1,33 @@
 import styled from "styled-components";
 import { px2vw } from "../../utils/px2vw";
 
+export const HandbagsStyle = styled.div`
+h1{
+  color: #1B4B66;
+  margin-left: 3vw;
+}
+    .containerMain{
+      display: flex;
+      justify-content: space-between;
+      margin: 3vw;
+  }
+  .containerCheckbox{
+    display: flex;
+    flex-direction: column;
+    width: 20vw;
+  }
+`;
 export const ProductListing = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   gap: ${px2vw(30)};
   justify-content: space-between;
-  width: 75%;
-  .containerCheckbox{
-    display: flex;
-  }
+  width: 69vw;
+
 `;
 export const ContainerProductListing = styled.div`
 display: flex;
 flex-direction: column;
+width: 69vw;
 `;
 
