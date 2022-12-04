f1f28e886b292be0a820a0363654e0ddd6889284
Jenyffer <jenyfferbastos06@gmail.com>
2022-12-04 02:12:54 -0300

    refatorei o arquivo main

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,7 +1,9 @@
+// .storybook/main.js
+
 module.exports = {
-  addons: ['@storybook/addon-essentials'],
-  framework: '@storybook/react',
-  stories: ['../src/*/.stories.@(tsx|mdx)'],
+  addons: ["@storybook/addon-essentials"],
+  framework: "@storybook/react",
+  stories: ["../src/*/.stories.@(tsx|mdx)"],
   webpackFinal: async (config, { configType }) => {
     // Make whatever fine-grained changes you need
     // Return the altered config
