380f850023b0a9bd80922d93f4e196f6bd0a9568
Jenyffer <jenyfferbastos06@gmail.com>
2022-12-04 02:11:32 -0300

    refatorei a estilização do componente Handbags

--- src/pages/Handbags/Handbags.ts
+++ src/pages/Handbags/Handbags.ts
@@ -1,7 +1,14 @@
 import styled from "styled-components";
 
 export const ProductListing = styled.div`
-  display: flex;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 30px;
   justify-content: space-between;
   width: 75%;
-`
+`;
+export const ContainerProductListing = styled.div`
+display: flex;
+flex-direction: column;
+`;
+
