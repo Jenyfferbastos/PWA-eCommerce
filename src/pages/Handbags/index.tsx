029a772b459cee6a37b662755a250cddd1480d9a
Jenyffer <jenyfferbastos06@gmail.com>
2022-12-04 15:12:51 -0300

    inseri novas categorias no menu de navegação

--- src/pages/Handbags/index.tsx
+++ src/pages/Handbags/index.tsx
@@ -17,7 +17,14 @@ export function Handbags(){
     <Header />
     <BannerHandbags />
     <Flex>
-    <SelectCheckbox  TextButton={"teste"} Icon={"Plus"}  />
+      <div className="containerCheckbox">
+    <SelectCheckbox  TextButton={"Size"} Icon={"Plus"} Name={['S','M','L','XL', 'XXL', '3XL', '4XL', '5XL']}  />
+    <SelectCheckbox  TextButton={"Color"} Icon={"Plus"} Name={['Blue','Maroon Red','Crimson Red','Seinna Pink', 'Teal', 'Aquamarine', 'Off-White', 'Muave Orange']}  />
+    <SelectCheckbox  TextButton={"Brand"} Icon={"Plus"} Name={['Zara','D & G','H & M','Chanel', 'Prada', 'Biba', 'CoraL', 'Other']}  />
+    <SelectCheckbox  TextButton={"Price Range"} Icon={"Plus"} Name={['$1 - $50','$51 - $100','$101 - $150','$151 - $200', '$201 - $250', '$251 - $300', '$301 - $350', '$351 - Max']}  />
+    <SelectCheckbox  TextButton={"Discount"} Icon={"Plus"} Name={['Mais de 5% OFF','Mais de 10% OFF','Mais de 15% OFF','Mais de 20% OFF', 'Mais de 25% OFF', 'Mais de 30% OFF', 'Mais de 35% OFF', 'Mais de 40% OFF']}  />
+    <SelectCheckbox  TextButton={"Availability"} Icon={"Plus"} Name={['Small','Medium','Large','Extra Large']}  />
+    </div>
     <ContainerProductListing>
     <ListingOptions />
     <ProductListing>
