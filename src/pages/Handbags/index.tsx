1078cfc3b2e7e07f31c8a71f9f4c605bf307f85d
Jenyffer <jenyfferbastos06@gmail.com>
2022-12-04 02:10:08 -0300

    refatorei a rota Handbags

--- src/pages/Handbags/index.tsx
+++ src/pages/Handbags/index.tsx
@@ -6,22 +6,31 @@ import ImagemBag1 from "../../assets/img/image-bag-1.png";
 import ImagemBag2 from "../../assets/img/image-bag-2.png";
 import ImagemBag3 from "../../assets/img/image-bag-3.png";
 import ImagemBag4 from "../../assets/img/image-bag-4.png";
-import { ProductListing } from "./Handbags";
+import { ContainerProductListing, ProductListing } from "./Handbags";
 import { Flex } from "../../components/ProductCard/ModelProductCardStyle";
+import { Footer } from "../../components/Footer/Footer";
+import { ListingOptions } from "../../components/ListingOptions/ListingOptions";
 
 export function Handbags(){
   return(
     <>
     <Header />
     <BannerHandbags />
-    <Flex >
+    <Flex>
     <SelectCheckbox  TextButton={"teste"} Icon={"Plus"}  />
+    <ContainerProductListing>
+    <ListingOptions />
     <ProductListing>
     <CardHorizontal img={ImagemBag1} nameBag={"Grande"} descritionBag={"Blossom Pouch"} valueBeforeBag={"$39.49"} valueAfterBag={"$78.66"} valueDiscountBag={"50% OFF"} rating={"43 Ratings"} />
     <CardHorizontal img={ImagemBag2} nameBag={"Grande"} descritionBag={"Blossom Pouch"} valueBeforeBag={"$39.49"} valueAfterBag={"$78.66"} valueDiscountBag={"50% OFF"} rating={"43 Ratings"} />
     <CardHorizontal img={ImagemBag3} nameBag={"Grande"} descritionBag={"Blossom Pouch"} valueBeforeBag={"$39.49"} valueAfterBag={"$78.66"} valueDiscountBag={"50% OFF"} rating={"43 Ratings"} />
+    <CardHorizontal img={ImagemBag3} nameBag={"Grande"} descritionBag={"Blossom Pouch"} valueBeforeBag={"$39.49"} valueAfterBag={"$78.66"} valueDiscountBag={"50% OFF"} rating={"43 Ratings"} />
+    <CardHorizontal img={ImagemBag3} nameBag={"Grande"} descritionBag={"Blossom Pouch"} valueBeforeBag={"$39.49"} valueAfterBag={"$78.66"} valueDiscountBag={"50% OFF"} rating={"43 Ratings"} />
+    <CardHorizontal img={ImagemBag3} nameBag={"Grande"} descritionBag={"Blossom Pouch"} valueBeforeBag={"$39.49"} valueAfterBag={"$78.66"} valueDiscountBag={"50% OFF"} rating={"43 Ratings"} />
     </ProductListing>
+    </ContainerProductListing>
     </Flex>
+    <Footer />
     </>
   )
 }
