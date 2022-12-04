7243e2f2f377b30e67d408488df0ced647953ecb
Jenyffer <jenyfferbastos06@gmail.com>
2022-12-04 15:12:31 -0300

    inseri novas categorias no menu de navegação

--- src/pages/Handbags/Handbags.ts
+++ src/pages/Handbags/Handbags.ts
@@ -1,11 +1,15 @@
 import styled from "styled-components";
+import { px2vw } from "../../utils/px2vw";
 
 export const ProductListing = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  gap: 30px;
+  gap: ${px2vw(30)};
   justify-content: space-between;
   width: 75%;
+  .containerCheckbox{
+    display: flex;
+  }
 `;
 export const ContainerProductListing = styled.div`
 display: flex;
