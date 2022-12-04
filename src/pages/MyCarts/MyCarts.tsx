52547936c7dcd56e1f3093ba472d4fe705c83163
Jenyffer <jenyfferbastos06@gmail.com>
2022-12-04 01:57:46 -0300

    criei o componente da rota My Carts

---
+++ src/pages/MyCarts/MyCarts.tsx
@@ -0,0 +1,61 @@
+import { Products } from "../../components/Products/Products";
+import imageBag2 from "../../assets/img/image-bag-2.png";
+import { Header } from "../../components/Header/Header";
+import { Footer } from "../../components/Footer/Footer";
+import { OrderSummary } from "../../components/OrderSummary/OrderSummary";
+import { MyCartsStyle } from "./MyCartsStyle";
+import { Cupom } from "../../components/Cupom/Cupom";
+import { CodeCheck } from "../../components/CodeCheck/CodeCheck";
+import { useState } from "react";
+import { Icons } from "../../components/Icons/Icons";
+
+export function MyCarts() {
+  const [show, setShow] = useState(false);
+  return (
+    <MyCartsStyle>
+      <Header />
+      <h1 className="titleMyCarts">My Cart</h1>
+      <div className="contentProduct">
+        <p>Product Name</p>
+        <div className="informationPriceProduct">
+          <p>Price</p>
+          <p>Qty</p>
+          <p>Subtotal</p>
+        </div>
+      </div>
+      <div className="cartContainer">
+        <div>
+          <Products
+            image={imageBag2}
+            nameProduct={"Coach"}
+            descriptionProduct={"Leather Coach Bag"}
+            quantityProduct={"Qty- 1"}
+            priceProduct={"$54.69"}
+            quantityProductNumber={"1"}
+            subtotalProduct={"$54.69"}
+          />
+
+          <Products
+            image={imageBag2}
+            nameProduct={"Coach"}
+            descriptionProduct={"Leather Coach Bag"}
+            quantityProduct={"Qty- 1"}
+            priceProduct={"$54.69"}
+            quantityProductNumber={"1"}
+            subtotalProduct={"$54.69"}
+          />
+        </div>
+        <div className="containerOrderSummary">
+          <OrderSummary />
+        </div>
+      </div>
+      <div className="couponCode">
+      <button className="selectCoupon" onClick={() => setShow(!show)}>
+        Apply Coupon Code <Icons icon="ChevronBottom" />
+      </button>
+      {show && <CodeCheck placeholder={"Apply Coupon Code"} />}
+      </div>
+      <Footer />
+    </MyCartsStyle>
+  );
+}
