23f25a0b38d1479bb5bca5b6d3e6e112d080122d
Jenyfferbastos <jenyfferbastos06@gmail.com>
2022-12-04 23:33:38 -0300

    corrigindo arquivos não utilizados

--- src/components/Icons/Icons.tsx
+++ src/components/Icons/Icons.tsx
@@ -2,23 +2,23 @@ import { generalIcons } from "./GeneralIcons";
 import { navigationIcons } from "./NavigationIcons";
 import { productIcons } from "./ProductIcons";
 
-export interface IconsProps{
+export interface IconsProps {
   icon: string;
 }
 
-export function Icons({icon}: IconsProps){
-    return(
-      <>
+export function Icons({ icon }: IconsProps) {
+  return (
+    <>
       <img src={determinesIcons(icon)} />
-      </>
-    );
+    </>
+  );
 }
 
-function determinesIcons(icon:string):string {
-
-const allIcons = {
-  ...generalIcons, ...navigationIcons, ...productIcons
-}
+function determinesIcons(icon: string): string {
+  const allIcons = {
+    ...generalIcons,
+    ...navigationIcons,
+    ...productIcons,
+  };
   return allIcons[icon];
 }
-
