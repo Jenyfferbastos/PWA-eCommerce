c1a137e51ece3419845abb6c41651b3dd7ccd523
Jenyffer <jenyfferbastos06@gmail.com>
2022-12-08 23:37:29 -0300

    Criando a rota Search para mobile

---
+++ src/pages/Search/Search.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {InputSearch} from "../../components/InputSearch/InputSearch"
+
+export function Search(){
+  return(
+    <>
+      <InputSearch placeholder={"Search"} />
+    </>
+  )
+}
