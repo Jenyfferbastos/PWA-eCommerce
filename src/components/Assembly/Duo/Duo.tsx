639a8c307e73eeb66021a9801a25702056154eb6
Jenyffer <jenyfferbastos06@gmail.com>
2022-12-08 23:28:10 -0300

    criando o componente Duo da Assembly

--- src/components/Assembly/Duo/Duo.tsx
+++ src/components/Assembly/Duo/Duo.tsx
@@ -1,9 +1,33 @@
-import { DuoStyle } from "./DuoStyle";
+import { Icons } from "../../Icons/Icons";
+import { DuoPrimaryStyle, DuoSecondaryStyle, DuoStyle } from "./DuoStyle";
 
-export function Duo(){
-  return(
+interface DuoProps {
+  sizeDuoPrimary: string;
+  sizeDuoSecondary: string;
+  textDuoPrimary: string;
+  iconDuoPrimary: string;
+  iconDuoSecondary: string;
+}
+
+export function Duo({
+  sizeDuoPrimary,
+  sizeDuoSecondary,
+  textDuoPrimary,
+  iconDuoPrimary,
+  iconDuoSecondary,
+}: DuoProps) {
+  return (
     <>
-    <DuoStyle  />
+      <DuoStyle>
+        <DuoSecondaryStyle sizeDuoSecondary={sizeDuoSecondary}>
+          <Icons icon={iconDuoSecondary} />
+        </DuoSecondaryStyle>
+
+        <DuoPrimaryStyle sizeDuoPrimary={sizeDuoPrimary}>
+          <Icons icon={iconDuoPrimary} />
+          {textDuoPrimary}
+        </DuoPrimaryStyle>
+      </DuoStyle>
     </>
-  )
+  );
 }
--- src/components/Assembly/Duo/DuoStyle.ts
+++ src/components/Assembly/Duo/DuoStyle.ts
@@ -1,5 +1,38 @@
 import styled from "styled-components";
 
+interface DuoPrimaryStyleProps {
+  sizeDuoPrimary: string;
+}
+interface DuoSecondaryStyleProps {
+  sizeDuoSecondary: string;
+}
 export const DuoStyle = styled.div`
-  width: ${props => `${props.size}`};
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const DuoPrimaryStyle = styled.button<DuoPrimaryStyleProps>`
+  width: ${(props) => props.sizeDuoPrimary};
+  height: 44px;
+  background-color: #1B4B66;
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+  align-items: center;
+  color: #FFFFFF;
+  font-size: 0.75rem;
+  font-weight: 600;
+  border: none;
+  border-radius: 8px;
+`;
+
+export const DuoSecondaryStyle = styled.button<DuoSecondaryStyleProps>`
+  width: ${(props) => props.sizeDuoSecondary};
+  height: 44px;
+  background-color: #F4F4F4;
+  display: flex;
+  border: none;
+  justify-content: center;
+  align-items: center;
+  border-radius: 8px;
 `;
