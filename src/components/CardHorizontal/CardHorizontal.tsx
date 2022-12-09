9159215fccf887cc409a31ce963aacd9b8a4abb4
Jenyffer <jenyfferbastos06@gmail.com>
2022-12-08 23:31:20 -0300

    refatorando a estilização do componente Card Horizontal

--- src/components/CardHorizontal/CardHorizontal.tsx
+++ src/components/CardHorizontal/CardHorizontal.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Icons } from "../Icons/Icons";
-import { Flex, ImageBag } from "../ProductCard/ModelProductCardStyle";
-import { CardHorizontalStyle, ValuesStyle } from "./CardHorizontalStyle";
+import { Flex } from "../ProductCard/ModelProductCardStyle";
+import { CardHorizontalStyle, ImageBag, ValuesStyle } from "./CardHorizontalStyle";
 
 export interface CardHorizontalProps {
   img: string;
--- src/components/CardHorizontal/CardHorizontalStyle.ts
+++ src/components/CardHorizontal/CardHorizontalStyle.ts
@@ -1,32 +1,43 @@
 import styled from "styled-components";
+import { mediaQueries } from "../../utils/mediaQueries";
 
+export const ImageBag = styled.img`
+  ${mediaQueries.desktop} {
+    width: ;
+  }
+  ${mediaQueries.mobile} {
+    width: 43vw;
+  }
+`
 export const CardHorizontalStyle = styled.div`
+${mediaQueries.desktop} {
   width: 332px;
 
   h4{
     color: #626262;
-    font-size: 14px;
+    font-size: 0.875rem;
     font-weight: 400;
   }
   .rating{
     color: #1B4B66;
-    font-size: 14px;
+    font-size: 0.875rem;
     font-weight: 500;
   }
   .valueBeforeBag{
     color: #171520;
-    font-weight: 500;
+    font-weight: 600;
     font-size: 16px;
   }
   .valueAfterBag{
     color: #626262;
     font-weight: 400;
-    font-size: 14px;
+    font-size: 0.875rem;
+    text-decoration-line: line-through;
   }
   .valueDiscountBag{
     color: #E21D1D;
     font-weight: 400;
-    font-size: 16px;
+    font-size: 1rem;
   }
   .containerRating{
     display: flex;
@@ -34,11 +45,58 @@ export const CardHorizontalStyle = styled.div`
     align-items: center;
     width: 200px;
   }
+}
+${mediaQueries.mobile} {
+  width: 43vw;
+  margin-bottom: 2vh;
+
+  h3{
+    color: #171520;
+    font-size: 0.75rem;
+    font-weight: 600;
+  }
+  h4{
+    color: #626262;
+    font-size: 0.75rem;
+    font-weight: 500;
+  }
+  .rating{
+    color: #1B4B66;
+    font-size: 0.875rem;
+    font-weight: 500;
+  }
+  .valueBeforeBag{
+    color: #171520;
+    font-weight: 500;
+    font-size: 0.875rem;
+  }
+  .valueAfterBag{
+    color: #626262;
+    font-weight: 600;
+    font-size: 0.625rem;
+    text-decoration-line: line-through;
+  }
+  .valueDiscountBag{
+    color: #E21D1D;
+    font-weight: 500;
+    font-size: 0.625rem;
+  }
+  .containerRating{
+  display: none;
+}}
 `;
 
 export const ValuesStyle = styled.div`
+${mediaQueries.desktop} {
   display: flex;
   justify-content: space-between;
   align-items: center;
   width: 200px;
+}
+${mediaQueries.mobile} {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 42vw;
+}
 `;
