ad67be34e7a63ff7f5e74d94653c384c8e562531
Jenyffer <jenyfferbastos06@gmail.com>
2022-12-14 23:45:20 -0300

    refatorando o componente Product Card

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -6,7 +6,7 @@ import { ContainerNavStyle, ProductCardStyle } from "./ProductCardStyle";
 import { Icons } from "../Icons/Icons";
 import { Product } from "../../interfaces/Products";
 import { listProducts } from "../../services/listProducts";
-import ImageBag1 from "../../assets/img/image-bag-1.png"
+import ImageBag1 from "../../assets/img/image-bag-1.png";
 import { Flex } from "./ModelProductCardStyle";
 import { Duo } from "../Assembly/Duo/Duo";
 
@@ -46,59 +46,65 @@ export function ProductCard() {
           if (index > 3) return null;
           return (
             <>
-            <Link to={product.pathUrl} key={index}>
-              <ModelProductCard
-                nameBag={product.name}
-                descritionBag={product.description}
-                valueBag={`$${product.pricingBefore}`}
-                img={product.imgLink}
-              />
-            </Link>
-                      <button onClick={toggleModal} className="btn-modal">teste
-                    </button>
+              <Link to={product.pathUrl} key={index}>
+                <ModelProductCard
+                  nameBag={product.name}
+                  descritionBag={product.description}
+                  valueBag={`$${product.pricingBefore}`}
+                  img={product.imgLink} key={index}
+                />
+              </Link>
+              <button onClick={toggleModal} className="btn-modal">
+                teste
+              </button>
             </>
           );
         })}
-        { modal &&(
-            <div className="modal">
+        {modal && (
+          <div className="modal">
             <div onClick={toggleModal} className="overlay"></div>
             <div className="modalContent">
               <div className="cardVertical">
                 <img src={ImageBag1} />
                 <div className="containerText">
-                <h2>Grande</h2>
-                <p className="descriptionProduct">Blossom Pouch</p>
-                <p className="priceProduct">$39.33</p>
+                  <h2>Grande</h2>
+                  <p className="descriptionProduct">Blossom Pouch</p>
+                  <p className="priceProduct">$39.33</p>
                 </div>
               </div>
-             <div className="ratings">
-              <div className="ratingsPill">
-                <p>4.5</p>
-                <Icons icon="Star" />
+              <div className="ratings">
+                <div className="ratingsPill">
+                  <p>4.5</p>
+                  <Icons icon="Star" />
+                </div>
+                <div className="content">
+                  <h3>Average Rating</h3>
+                  <p>43 Ratings & 23 Reviews</p>
+                </div>
+                <Icons icon="ChevronRight" />
               </div>
-              <div className="content">
-              <h3>Average Rating</h3>
-              <p>43 Ratings & 23 Reviews</p>
+              <div className="selectOptions">
+                <div className="containerText">
+                  <h4>Select Size</h4>
+                  <p>( UK Size)</p>
+                </div>
+                <div className="buttons">
+                  <button>7</button>
+                  <button>8</button>
+                  <button>9</button>
+                  <button>10</button>
+                </div>
               </div>
-              <Icons icon="ChevronRight" />
-             </div>
-             <div className="selectOptions">
-              <div className="containerText">
-              <h4>Select Size</h4>
-              <p>( UK Size)</p>
-              </div>
-              <div className="buttons">
-              <button>7</button>
-              <button>8</button>
-              <button>9</button>
-              <button>10</button>
-              </div>
-             </div>
-              <Duo sizeDuoPrimary={'72vw'} sizeDuoSecondary={'14vw'} textDuoPrimary={"Add to Bag"} iconDuoPrimary={"bagFillFalseWhite"} iconDuoSecondary={"Placeholder"} />
+              <Duo
+                sizeDuoPrimary={"72vw"}
+                sizeDuoSecondary={"14vw"}
+                textDuoPrimary={"Add to Bag"}
+                iconDuoPrimary={"bagFillFalseWhite"}
+                iconDuoSecondary={"Placeholder"}
+              />
             </div>
           </div>
-        )
-        }
+        )}
       </ProductCardStyle>
     </>
   );
