2967e867a1ad2a7e4a9e185a1cc87f0ddb45049c
Jenyffer <jenyfferbastos06@gmail.com>
2022-12-14 23:45:53 -0300

    refatorando o componente Product Page

--- src/components/ProductPage/ProductPage.tsx
+++ src/components/ProductPage/ProductPage.tsx
@@ -5,7 +5,13 @@ import { Footer } from "../Footer/Footer";
 import { Icons } from "../Icons/Icons";
 import { CodeCheck } from "../CodeCheck/CodeCheck";
 import { RatingsAndStepper } from "../RatingsAndStepper/RatingsAndStepper";
-import { ProductDescription, ProductPageStyle } from "./ProductPageStyle";
+import {
+  AppBar,
+  ProductDescription,
+  ProductPageStyle,
+} from "./ProductPageStyle";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export interface ProductPageProps {
   img: string;
@@ -26,83 +32,110 @@ export function ProductPage({
   pricingDiscount,
   ratings,
 }: ProductPageProps) {
+  const [show, setShow] = useState(false);
   return (
     <ProductPageStyle>
       <div className="containerMain">
-      <div className="containerProductPics">
-        <img src={img} />
-      </div>
-      <div className="containerProductInfo">
-        <div className="containerProductNameAndDescription">
-          <h1>{productName}</h1>
-          <p>{productDescription}</p>
+        <AppBar>
+          <Link to="/Handbags">
+            <Icons icon="ChevronLeft" />
+          </Link>
+        </AppBar>
+        <div className="containerProductPics">
+          <img src={img} />
         </div>
-        <div className="containerRatings">
-          <div>
-            <Icons icon={"Star"} />
-            <Icons icon={"Star"} />
-            <Icons icon={"Star"} />
-            <Icons icon={"Star"} />
-            <Icons icon={"Star"} />
+        <div className="containerProductInfo">
+          <div className="containerProductNameAndDescription">
+            <h1>{productName}</h1>
+            <p>{productDescription}</p>
           </div>
-          <p>{ratings}</p>
-        </div>
-        <div className="productPricing">
-          <p className="pricingBefore">{pricingBefore}</p>
-          <p className="pricingAfter">{pricingAfter}</p>
-          <p className="pricingDiscount">{pricingDiscount}</p>
-        </div>
-        <div className="pinCodeCheck">
-          <div className="textPinCodeCheck">
-            <h3>Delivery Details</h3>
-            <p>
-              Check estimated delivery<br></br> date/pickup option.
-            </p>
+          <div className="containerRatings">
+            <div>
+              <Icons icon={"Star"} />
+              <Icons icon={"Star"} />
+              <Icons icon={"Star"} />
+              <Icons icon={"Star"} />
+              <Icons icon={"Star"} />
+            </div>
+            <p>{ratings}</p>
           </div>
-          <CodeCheck placeholder={"Apply Valid Pincode"} />
+          <div className="productPricing">
+            <p className="pricingBefore">{pricingBefore}</p>
+            <p className="pricingAfter">{pricingAfter}</p>
+            <p className="pricingDiscount">{pricingDiscount}</p>
+          </div>
+
+          <div className="ratings">
+            <div className="ratingsPill">
+              <p>4.5</p>
+              <Icons icon="Star" />
+            </div>
+            <div className="content">
+              <h3>Average Rating</h3>
+              <p>43 Ratings & 23 Reviews</p>
+            </div>
+          </div>
+
+          <div className="pinCodeCheck">
+            <div className="textPinCodeCheck">
+              <h3>Delivery Details</h3>
+              <p>Check estimated delivery date/pickup option.</p>
+            </div>
+            <CodeCheck placeholder={"Apply Valid Pincode"} />
+          </div>
+          <div className="quantity">
+            <h4>Quantity:</h4>
+            <RatingsAndStepper />
+          </div>
+          <div className="containerCupom">
+            <Cupom />
+            <Cupom />
+          </div>
+          <div className="buttonsAdd">
+            <Single icon={"bagFillFalseWhite"} label={"Add to bag"} />
+            <ButtonMedium
+              icon={"wishlistFillFalse"}
+              label={"Add To Wishlist"}
+            />
+          </div>
         </div>
-        <div className="quantity">
-          <h4>Quantity:</h4>
-          <RatingsAndStepper />
-        </div>
-        <div className="containerCupom">
-          <Cupom />
-          <Cupom />
-        </div>
-        <div className="buttonsAdd">
-        <Single icon={"bagFillFalseWhite"} label={"Add to bag"} />
-        <ButtonMedium icon={"wishlistFillFalse"} label={"Add To Wishlist"} />
-        </div>
-      </div>
       </div>
       <ProductDescription>
         <div className="buttonsProductDescription">
-        <button>Product Description</button>
-        <button>Related Products</button>
-        <button>Ratings and Reviews</button>
+          <button onClick={() => setShow(!show)}>Product Description <Icons icon="ChevronBottom" /></button>
+          {show && (
+            <p>
+              Experience comfortable and easy travelling like never before with
+              this coach bag. It features a zip closure, removable straps and
+              multiple organization compartments to keep your valuables safe.
+              Crafted from premium material, it is durable and lasts long.
+            </p>
+          )}
+          <button>Related Products</button>
+          <button>Ratings and Reviews</button>
         </div>
         <div className="textDescription">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risus
-          scelerisque laoreet tortor cras molestie tincidunt malesuada
-          malesuada. Neque, mauris duis dui id morbi magna. Cras lacus, viverra
-          auctor in turpis est quisque eget tristique.{" "}
-        </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risus
+            scelerisque laoreet tortor cras molestie tincidunt malesuada
+            malesuada. Neque, mauris duis dui id morbi magna. Cras lacus,
+            viverra auctor in turpis est quisque eget tristique.{" "}
+          </p>
 
-        <p>
-          Dolor augue mattis duis semper gravida enim eu imperdiet sit. Et
-          pharetra platea pretium nec feugiat tincidunt quam leo tristique.
-          Nulla enim consectetur sit et tempus, faucibus leo ac cras. Purus ut
-          non eu mus volutpat.{" "}
-        </p>
+          <p>
+            Dolor augue mattis duis semper gravida enim eu imperdiet sit. Et
+            pharetra platea pretium nec feugiat tincidunt quam leo tristique.
+            Nulla enim consectetur sit et tempus, faucibus leo ac cras. Purus ut
+            non eu mus volutpat.{" "}
+          </p>
 
-        <p>
-          Eget est vel sagittis amet sit eu eu ullamcorper tellus. Leo mauris,
-          faucibus vulputate adipiscing elementum tristique dictumst augue
-          pellentesque. Justo, sed nunc, pretium turpis scelerisque. Enim urna
-          etiam morbi vestibulum ac dictumst. Ac ut elementum molestie sit felis
-          imperdiet.
-        </p>
+          <p>
+            Eget est vel sagittis amet sit eu eu ullamcorper tellus. Leo mauris,
+            faucibus vulputate adipiscing elementum tristique dictumst augue
+            pellentesque. Justo, sed nunc, pretium turpis scelerisque. Enim urna
+            etiam morbi vestibulum ac dictumst. Ac ut elementum molestie sit
+            felis imperdiet.
+          </p>
         </div>
       </ProductDescription>
       <Footer />
